e8b8a16a8121149b0ce8041fbe999d26b45610a8
Jose Naranjo <naranjja@gmail.com>
2016-04-07 01:59:47 -0500

    Added content to main activity and icon

--- Android/app/src/main/java/com/example/jose/chismesup/activity/MainActivity.java
+++ Android/app/src/main/java/com/example/jose/chismesup/activity/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.jose.chismesup.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.example.jose.chismesup.R;
 
 /**
  * Main Activity
@@ -8,4 +16,56 @@ import android.app.Activity;
  */
 
 public class MainActivity extends Activity {
+
+    FrameLayout bSocial;
+    FrameLayout bCourses;
+    ImageView camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        bSocial = (FrameLayout) findViewById(R.id.bSocial);
+        bCourses = (FrameLayout) findViewById(R.id.bCourses);
+        camera = (ImageView) findViewById(R.id.camera);
+
+        bSocial.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                hightlightButton(bSocial);
+                unhightlightButton(bCourses);
+            }
+        });
+
+        bCourses.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                hightlightButton(bCourses);
+                unhightlightButton(bSocial);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void hightlightButton(FrameLayout button) {
+        button.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLighter));
+    }
+
+    private void unhightlightButton(FrameLayout button) {
+        button.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+    }
+
 }
--- Android/app/src/main/java/com/example/jose/chismesup/activity/ValidationActivity.java
+++ Android/app/src/main/java/com/example/jose/chismesup/activity/ValidationActivity.java
@@ -3,9 +3,13 @@ package com.example.jose.chismesup.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.jose.chismesup.R;
@@ -17,13 +21,28 @@ import com.example.jose.chismesup.R;
 
 public class ValidationActivity extends Activity {
 
+    FrameLayout bValidate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_validation);
 
-        findViewById(R.id.bValidate).setOnClickListener(new LoginClickListener());
+        bValidate = (FrameLayout) findViewById(R.id.bValidate);
+        bValidate.setOnClickListener(new LoginClickListener());
+
+        EditText etStudentId = (EditText) findViewById(R.id.etStudentID);
+        etStudentId.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    bValidate.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     private void launchMainActivity() {
@@ -35,11 +54,16 @@ public class ValidationActivity extends Activity {
     private class LoginClickListener implements View.OnClickListener {
 
         @Override
-        public void onClick(View v) {
+        public void onClick(View view) {
+
+            hightlightButton(bValidate);
+
             if (validateUser()) {
                 launchMainActivity();
+
             } else {
                 showErrorMessage();
+                unhightlightButton(bValidate);
             }
         }
 
@@ -53,6 +77,14 @@ public class ValidationActivity extends Activity {
 
     private void showErrorMessage() {
         Toast.makeText(this, getString(R.string.invalid_user), Toast.LENGTH_LONG).show();
+    }
+
+    private void hightlightButton(FrameLayout button) {
+        button.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLighter));
+    }
+
+    private void unhightlightButton(FrameLayout button) {
+        button.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
     }
 
 }
